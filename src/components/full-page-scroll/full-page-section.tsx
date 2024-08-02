156782cdfafd5ac75e027424617aafd5399ffb92
Thiago Elias <thiagoelias99@gmail.com>
2024-08-02 13:03:16 -0300

    Scroll notification

--- src/components/full-page-scroll/full-page-section.tsx
+++ src/components/full-page-scroll/full-page-section.tsx
@@ -2,15 +2,17 @@ import { cn } from '@/lib/utils'
 import useIntersectionObserver from '@/useIntersectionObserver'
 import { useEffect, useRef } from 'react'
 import { ClassNameValue } from 'tailwind-merge'
+import SectionScrollDown from '../ui/section-scroll-down'
 
 interface FullPageSectionProps {
   id: string
   className?: ClassNameValue
   children?: React.ReactNode
   setActiveSection?: (section: string) => void
+  disableScrollDownNotification?: boolean
 }
 
-export default function FullPageSection({ id, children, className, setActiveSection }: FullPageSectionProps) {
+export default function FullPageSection({ id, children, className, setActiveSection, disableScrollDownNotification = false }: FullPageSectionProps) {
 
   const sectionRef = useRef(null)
   const isVisible = useIntersectionObserver(sectionRef, { threshold: 0.1 })
@@ -28,6 +30,7 @@ export default function FullPageSection({ id, children, className, setActiveSect
       className={cn('bg-transparent flex flex-col items-start justify-start w-full h-svh [scroll-snap-align:_start]', className)}
     >
       {children}
+      {!disableScrollDownNotification && <SectionScrollDown />}
     </section>
   )
 }
---
+++ src/components/ui/section-scroll-down.tsx
@@ -0,0 +1,15 @@
+import { cn } from '@/lib/utils'
+import { ChevronsDownIcon } from 'lucide-react'
+import { ClassNameValue } from 'tailwind-merge'
+
+interface ISectionScrollDownProps {
+  className?: ClassNameValue
+}
+
+export default function SectionScrollDown({ className }: ISectionScrollDownProps) {
+  return (
+    <div className={cn('w-full mt-10 mb-20', className)}>
+      <ChevronsDownIcon className='w-12 h-12 text-foreground mx-auto mt-4 animate-bounce' />
+    </div>
+  )
+}
