5afb27ee946187d1dbb9744ceca7eecfe8dffbc6
Thiago Elias <thiagoelias99@gmail.com>
2025-01-10 08:45:12 -0300

    create ve project

---
+++ v2/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-full flex justify-center items-center bg-slate-200">
+      <h1 className="text-red-500">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
